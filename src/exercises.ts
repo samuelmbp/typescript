238e5e4c8a627575aecd5d6ce5ec267e09427a71
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-01 09:12:01 +0100

    Fundamentals: functions, objects and exercises

---
+++ src/exercises.ts
@@ -0,0 +1,28 @@
+// ===============
+/** DONE: FUNDAMENTALS*/
+// ===============
+/** 1. For each of these values, what type will the TypeScript compiler infer? */
+let a = 100; // Number
+let b = 'Coffee'; // String
+let c = [true, false, false]; // Boolean
+let d = {age: number}; // { age: 20}
+let e = [3]; // number[]
+let f; // any
+let g = []; // any[]
+
+/** 2. What are the compilation errors in each of the following code snippets? */
+// ==========================================================================================
+// let song: { title: string, releaseYear: number} = { title:'My song'}; // error: releaseYear must be assigned a value or ''
+let song: { title: string, releaseYear: number} = { title:'My song', releaseYear: 2022};  // Good
+
+// ==========================================================================================
+let prices = [100, 200, 300]; // number[]
+// prices[0] = '$100'; // error (cannot contain a string)
+prices[0] = 100 // Good
+
+// ==========================================================================================
+// function myFunc(a:number, b:number):number {} //error: must use the arguments
+
+function myFunc(a:number, b:number):number { // Good
+    return a * b;
+}
--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ let level: string;
 // =============================
 /** 'Any' type */
 // =============================
-// function render(document: any) {
-//     console.log(document)
-// }
+function render(document: any) {
+    console.log(document)
+}
 
 // =============================
 /** 'Arrays' */
@@ -24,3 +24,38 @@ let level: string;
 // =============================
 let user: [number, string] = [10, 'Samuel'];
 user.push(1); // can add an extra value in this way (as it is an array) -> not the best.
+
+// =============================
+/** 'Enums' -> related constants */
+// =============================
+// enum Size { Small, Medium, Large };
+const enum Size { Small = 1, Medium, Large }; // Using const cleans the compiled code e.g. dist/index.js
+const mySize: Size = Size.Small
+console.log(mySize); // 1
+
+
+// ====================================
+/** 'Functions' ->  */
+// ====================================
+function calculateTax(income: number, taxYear: number = 2022): number { // taxYear is optional
+    if (taxYear < 2022) 
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(10_000);
+
+
+// ====================================
+/** 'Objects' ->  */
+// ====================================
+// let employee: { id: number, name?: string} = { id: 1, name: '' }; // Name is optional (?)
+// let employee: { id: number, name: string} = { id: 1, name: '' };
+// employee.name = 'Sam'; 
+
+// Read Only
+let employee: { readonly id: number, name: string, retire: (date: Date) => void } = 
+    { id: 1, name: '', retire: (date: Date) => console.log(date.getDay())};
+// employee.id = 2; // error
+
+
