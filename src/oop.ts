cc95759aa874f9947285d12932954fb520f62558
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-04 12:14:54 +0100

    OOP: access control keywords, parameter properties, getter and setters

--- src/oop.ts
+++ src/oop.ts
@@ -1,21 +1,39 @@
 class Account {
-	id: number;
-	owner: string;
-	balance: number;
+	// readonly id: number; // Can only be set in the constructor
+	// owner: string;
+	// private _balance: number;
+	// nickname?: string; // Optional (?)
 
-	constructor(id: number, owner: string, balance: number) {
-		this.id = id;
-		this.owner = owner;
-		this.balance = balance;
-	}
+	// constructor(id: number, owner: string, balance: number) {
+	// 	this.id = id;
+	// 	this.owner = owner;
+	// 	this._balance = balance;
+	// }
+
+	// IMPORTANT: Shorter syntax (the compiler creates the properties at run time)
+	constructor(
+		public readonly id: number,
+		public owner: string,
+		private _balance: number
+	) {} // Perameter Properties
 
 	deposit(amount: number): void {
 		if (amount <= 0) throw new Error('Invalid amount.');
-		this.balance += amount;
+		this._balance += amount;
 	}
+
+	get balance(): number {
+		return this._balance;
+	}
+
+	// set balance(value: number) {
+	// 	if (value < 0) throw new Error('Invalid value.');
+	// 	this._balance = value;
+	// }
 }
 
 const account = new Account(1, 'Samuel', 0);
 account.deposit(100);
 console.log(account instanceof Account); // True
 console.log(typeof account); // Object
+console.log(account.balance); // Getter
