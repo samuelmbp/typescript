e1e8a5b1f208e35f1325ee6b19630cde0c9c8873
Samuel Raducan <samuel.thembp@gmail.com>
2022-08-04 12:48:37 +0100

    OOP: index signatures and static members

--- src/oop.ts
+++ src/oop.ts
@@ -34,6 +34,57 @@ class Account {
 
 const account = new Account(1, 'Samuel', 0);
 account.deposit(100);
-console.log(account instanceof Account); // True
-console.log(typeof account); // Object
-console.log(account.balance); // Getter
+// console.log(account instanceof Account); // True
+// console.log(typeof account); // Object
+// console.log(account.balance); // Getter
+
+// =======================================================
+/** Index Signatures -> Creates properties dynamically */
+// =======================================================
+class SeatAssignment {
+	// Index Signature Property
+	[seatNumber: string]: string;
+}
+
+const seats = new SeatAssignment();
+seats.A1 = 'Samuel';
+seats['A1'] = 'Samuel';
+seats.A2 = 'Ilaria';
+// seats.A2 = 2; // Error
+
+// =======================================================
+/** Static Members */
+// =======================================================
+class Ride {
+    // Global Usage
+	private static _activeRides: number = 0; // Can be used only by the 'Ride Class' -> e.g. (Ride._activeRides)
+
+	start() {
+		Ride._activeRides++;
+	}
+    
+	stop() {
+		Ride._activeRides--;
+	}
+
+     static get activeRides() {
+        return Ride._activeRides;
+    }
+}
+
+// Ride.activeRides = 10; // Read only -> Cannot be modified
+const ride1 = new Ride();
+ride1.start();
+
+const ride2 = new Ride();
+ride2.start();
+
+console.log(`First Ride: ${Ride.activeRides}`); // 2
+
+
+
+
+
+
+
+
